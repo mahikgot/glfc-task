5bbd80ed54ebd9b4e7fe1d4449fe3efe0777d320
Debian <debian@vps-bade4935.vps.ovh.ca>
2024-03-15 06:56:01 +0000

    finish init implementation of loan

--- loanflask/routes.py
+++ loanflask/routes.py
@@ -9,16 +9,30 @@ loan_bp = Blueprint("loan_bp", __name__)
 def loan():
     if not request.is_json:
         return "415 Unsupported Media Type", 415
-    try:
-        newUser, loan = RequestSchema().load(request.json)
-        user = db.session.query(User).filter_by(name=newUser.name).first()
-        if user == None:
-            user = newUser
-            db.session.add(user)
-        user.loans.append(loan)
-        db.session.add(loan)
-        db.session.commit()
-    except ValidationError as err:
-        return err.messages, 400
-    return "Success", 200
 
+    errors = RequestSchema().validate(request.json)
+    if errors:
+        return errors, 400
+
+    newUser, loan = RequestSchema().load(request.json)
+    user = db.session.query(User).filter_by(name=newUser.name).first()
+    if user == None:
+        user = newUser
+        db.session.add(user)
+    user.loans.append(loan)
+    db.session.add(loan)
+    db.session.commit()
+
+    r = .02
+    monthlyPayment = (loan.amount * r*((1+r)**loan.term)) / (((1+r)**loan.term)-1)
+    totalInterest  = monthlyPayment * loan.term
+    res = {
+            "Principal Loan Amount": loan.amount,
+            "Monthly Payment Amount": monthlyPayment,
+            "Total Interest Amount": totalInterest,
+            "Loan Term": loan.term,
+            "Total Sum of Payments": loan.amount+totalInterest
+            }
+
+
+    return res
